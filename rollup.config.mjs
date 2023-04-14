b66f6ac246d9b5e7015ffe13eeeb1760ca7be668
margolisj <jmarg@sas.upenn.edu>
2023-04-14 17:44:06 -0400

    Roll up and tsconfig warnings clean up.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,9 @@
-// import typescript from 'rollup-plugin-typescript2';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import sass from 'rollup-plugin-sass';
 
-// import pkg from './package.json';
 const pkg = {
   main: 'dist/index.js',
   module: 'dist/index/es/js'
@@ -36,16 +34,7 @@ export default {
       // clean: true
     }),
     commonjs({
-      include: ['node_modules/**'],
-      namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement'
-        ],
-        'node_modules/react-dom/index.js': ['render']
-      }
+      include: ['node_modules/**']
     })
   ]
 };
