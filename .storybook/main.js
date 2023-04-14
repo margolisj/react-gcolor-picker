dad43d4449cffea51af68a48d65caade36399ea5
margolisj <jmarg@sas.upenn.edu>
2023-04-14 17:02:44 -0400

    Updates storybook, adds babel presets, updates enzyme plugin for 18.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
   "stories": [
-    "../stories/docs/*.stories.mdx",
+    // "../stories/docs/*.stories.mdx",
     "../stories/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-scss"
-  ]
-}
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
