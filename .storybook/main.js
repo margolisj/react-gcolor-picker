699cb258530a65829a8dcf22c02c8e964e071f89
margolisj <jmarg@sas.upenn.edu>
2023-04-17 14:52:59 -0400

    Removes @storybook/addon-mdx-gfm and cleanup.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/docs/*.stories.mdx",
     "../stories/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss", "@storybook/addon-mdx-gfm"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
