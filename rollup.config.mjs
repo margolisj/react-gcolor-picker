f1c3756d8183adf4ea0c000387bed4ab3f19c4b8
margolisj <jmarg@sas.upenn.edu>
2023-08-02 19:05:30 -0400

    More rollup fixes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,46 @@
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import external from 'rollup-plugin-peer-deps-external';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import sass from 'rollup-plugin-sass';
 
 import pkg from './package.json' assert { type: 'json' };
 
-export default [
-  {
-    input: 'src/index.tsx',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      sass()
-    ]
-  }
-];
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    nodeResolve(),
+    sass({ insert: true }),
+    typescript({
+      // rollupCommonJSResolveHack: true,
+      clean: true
+    }),
+    commonjs({
+      include: ['node_modules/**']
+      // namedExports: {
+      //   'node_modules/react/react.js': [
+      //     'Children',
+      //     'Component',
+      //     'PropTypes',
+      //     'createElement'
+      //   ],
+      //   'node_modules/react-dom/index.js': ['render']
+      // }
+    })
+  ]
+};
