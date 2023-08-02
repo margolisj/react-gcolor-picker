899ee7bf40e4082c1943f1b7188f4656b5198cc4
margolisj <jmarg@sas.upenn.edu>
2023-08-02 17:43:40 -0400

    Simple one to try.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,48 +1,32 @@
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import sass from 'rollup-plugin-sass';
-
-// const pkg = {
-//   main: 'dist/index.js',
-//   module: 'dist/index/es/js'
-// };
 
 import pkg from './package.json' assert { type: 'json' };
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    nodeResolve(),
-    typescript({ tsconfig: './tsconfig.json' }),
-    sass({ insert: true }),
-    commonjs({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      include: /node_modules/
-    })
-    // typescript({
-    //   // rollupCommonJSResolveHack: true,
-    //   // clean: true
-    // })
-    // commonjs({
-    //   include: ['node_modules/**'],
-    //   extensions: ['.js', '.ts']
-    // })
-  ]
-};
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      sass()
+    ],
+  }
+];
