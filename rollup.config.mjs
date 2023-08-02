0a08de76dba4e0d9347c54f6d03064731a948772
margolisj <jmarg@sas.upenn.edu>
2023-08-02 17:49:01 -0400

    Lint fix.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import sass from 'rollup-plugin-sass';
 
@@ -8,25 +8,25 @@ import pkg from './package.json' assert { type: 'json' };
 
 export default [
   {
-    input: "src/index.tsx",
+    input: 'src/index.tsx',
     output: [
       {
         file: pkg.main,
-        format: "cjs",
-        sourcemap: true,
+        format: 'cjs',
+        sourcemap: true
       },
       {
         file: pkg.module,
-        format: "esm",
-        sourcemap: true,
-      },
+        format: 'esm',
+        sourcemap: true
+      }
     ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: './tsconfig.json' }),
       sass()
-    ],
+    ]
   }
 ];
